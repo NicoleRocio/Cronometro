3af8324c2e1bc9e6dff1b8ce1bdb6ca723c680c7
Jr. SoftNanTec <nandno1408@gmail.com>
2025-10-15 21:51:13 -0500

    feat: 20 instituciones

--- Cronometro/src/libs/config/instituciones.js
+++ Cronometro/src/libs/config/instituciones.js
@@ -39,6 +39,126 @@ export const instituciones = [
     logo2: "/src/assets/logo2.png",
     alumnos: "/src/assets/alumnos.png",
     colorFondo: "bg-green-900"
+  },
+  {
+    id: "institucion1",
+    nombre: "Cientifica",
+    logo: "/src/assets/logo.png",
+    logo2: "/src/assets/logo2.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-sky-800"
+  },
+  {
+    id: "institucion2", 
+    nombre: "Institución 2",
+    logo: "/src/assets/logo2.png",
+    logo2: "/src/assets/logo.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-blue-900"
+  },
+  {
+    id: "institucion3",
+    nombre: "Institución 3", 
+    logo: "/src/assets/logo.png",
+    logo2: "/src/assets/logo2.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-indigo-900"
+  },
+  {
+    id: "institucion4",
+    nombre: "Institución 4",
+    logo: "/src/assets/logo2.png",
+    logo2: "/src/assets/logo.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-purple-900"
+  },
+  {
+    id: "institucion5",
+    nombre: "Institución 5",
+    logo: "/src/assets/logo.png",
+    logo2: "/src/assets/logo2.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-green-900"
+  },
+  {
+    id: "institucion1",
+    nombre: "Cientifica",
+    logo: "/src/assets/logo.png",
+    logo2: "/src/assets/logo2.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-sky-800"
+  },
+  {
+    id: "institucion2", 
+    nombre: "Institución 2",
+    logo: "/src/assets/logo2.png",
+    logo2: "/src/assets/logo.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-blue-900"
+  },
+  {
+    id: "institucion3",
+    nombre: "Institución 3", 
+    logo: "/src/assets/logo.png",
+    logo2: "/src/assets/logo2.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-indigo-900"
+  },
+  {
+    id: "institucion4",
+    nombre: "Institución 4",
+    logo: "/src/assets/logo2.png",
+    logo2: "/src/assets/logo.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-purple-900"
+  },
+  {
+    id: "institucion5",
+    nombre: "Institución 5",
+    logo: "/src/assets/logo.png",
+    logo2: "/src/assets/logo2.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-green-900"
+  },
+  {
+    id: "institucion1",
+    nombre: "Cientifica",
+    logo: "/src/assets/logo.png",
+    logo2: "/src/assets/logo2.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-sky-800"
+  },
+  {
+    id: "institucion2", 
+    nombre: "Institución 2",
+    logo: "/src/assets/logo2.png",
+    logo2: "/src/assets/logo.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-blue-900"
+  },
+  {
+    id: "institucion3",
+    nombre: "Institución 3", 
+    logo: "/src/assets/logo.png",
+    logo2: "/src/assets/logo2.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-indigo-900"
+  },
+  {
+    id: "institucion4",
+    nombre: "Institución 4",
+    logo: "/src/assets/logo2.png",
+    logo2: "/src/assets/logo.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-purple-900"
+  },
+  {
+    id: "institucion5",
+    nombre: "Institución 5",
+    logo: "/src/assets/logo.png",
+    logo2: "/src/assets/logo2.png",
+    alumnos: "/src/assets/alumnos.png",
+    colorFondo: "bg-green-900"
   }
 ];
 
